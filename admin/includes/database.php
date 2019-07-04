8cbfbf250efdd5813f35cad9f92c8723ae072d4d
Tina <hnewbu1@gmail.com>
2019-07-04 12:50:07 -0400

    Upload initial files

---
+++ admin/includes/database.php
@@ -0,0 +1,62 @@
+<?php 
+
+require_once("new_config.php");
+
+class Database {
+
+
+
+    public $dbc;
+    function __construct() {
+
+        $this->open_db_connection();
+    }
+
+
+    public function open_db_connection() {
+        try {
+
+        $this->dbc = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+            // set the PDO error mode to exception
+        $this->dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //return $this->dbc;
+
+        }catch(PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+    }
+    public function query($sql){
+        try {
+            $result = $this->dbc->prepare($sql);
+            $result->execute();
+            return $result;
+        }catch(PDOException $e){
+            echo "Query failed: " . $e->getMessage();
+        }
+    }
+    // private function confirm_query($result) {
+    //     if(!$result){
+    //         die("Query Failed");
+    //     }
+
+    // }
+    public function escape_string($string) {
+       $escaped_string = htmlspecialchars(trim($string));
+       return $escaped_string;
+    }
+    public function the_insert_id() {
+        $this->dbc->lastInsertID();
+    }
+
+}
+
+$database = new Database();
+//$database->open_db_connection();
+
+
+
+
+ 
+
+?>
---
+++ admin/includes/new_config.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL); //check all type of errors
+ini_set('display_errors',1); // display those if any happen
+
+//Database Connection Constant
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','root');
+define('DB_NAME','gallery_db');
+
+
+?>
